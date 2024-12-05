1466bec6b7c86dd642a39edcbea1a91097686178
Christian Almazan <300088753@fvtc.edu>
2024-12-05 15:51:29 -0600

    added event api routes + endpoints.rest

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -20,4 +20,21 @@ router.get('/', async (request, response) => {
     else response.send({ error: { message: `Could not find events` }})
 })
 
+router.get('/:id', async (request, response) => {
+    const id = request.params
+
+    const events = await getEvents()
+    const foundEvents = await events.findOne({_id: new ObjectId(id)})
+    if (foundEvents) response.send(foundEvents)
+    else response.send({ error: { message: `Could not find event with id: ${id}` }})
+})
+
+router.post('/', async (request, response) => {
+    const { name, location, date, time } = request.body
+
+    const events = await getEvents()
+    const inserted = await events.insertOne({ name, location, date, time })
+    response.send(inserted)
+})
+
 module.exports = router
