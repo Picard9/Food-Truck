0413dd2da5c90c685d1c6da0fa5a1333323a786f
Christian Almazan <300088753@fvtc.edu>
2024-12-05 14:48:39 -0600

    added static page, and event endpoint

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -1,1 +1,23 @@
+
+
+const router = require('express').Router()
+
+const { response } = require('express')
 const { getCollection, ObjectId } = require('../../../dbconnect')
+
+let collection = null
+const getEvents = async () => {
+    if (!collection) collection = await getCollection('FoodTruckAPI', 'Events')
+    return collection
+}
+
+// API Routes
+
+router.get('/', async (request, response) => {
+    const events = await getEvents()
+    const foundEvents = await events.find().toArray()
+    if (foundEvents) response.send(foundEvents)
+    else response.send({ error: { message: `Could not find events` }})
+})
+
+module.exports = router
