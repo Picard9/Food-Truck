d5c7d99fc8db15b5708926c342365e64f307b6aa
Emilien Aboula <300095450@fvtc.edu>
2024-12-07 07:43:06 -0800

    Home page updated

--- dbconnect.js
+++ dbconnect.js
@@ -1,12 +1,12 @@
-const { MongoClient, ObjectId } = require('mongodb')
+// const { MongoClient, ObjectId } = require('mongodb')
 
-const { uri } = require('./secrets/mongodb.json')
+// const { uri } = require('./secrets/mongodb.json')
 
-const client = new MongoClient(uri)
+// const client = new MongoClient(uri)
 
-const getCollection = async (dbName, collectionName) => {
-    await client.connect()
-    return client.db(dbName).collection(collectionName)
-}
+// const getCollection = async (dbName, collectionName) => {
+//     await client.connect()
+//     return client.db(dbName).collection(collectionName)
+// }
 
-module.exports = { getCollection, ObjectId }
+// module.exports = { getCollection, ObjectId }
